a92f1cb9549dc7efdafff7c97afbd7397e29d565
ramesh900 <rameshsapkota900@gmail.com>
2025-09-25 01:05:03 -0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import { RouterProvider } from "react-router-dom";
 import "../index.css";
 
 const basename = import.meta.env.MODE === 'production' ? '/dhrin900.github.io' : '';
+console.log('Environment:', import.meta.env.MODE);
+console.log('Base URL:', basename);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} basename={basename} />
   </React.StrictMode>
 );
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} basename={basename} />
