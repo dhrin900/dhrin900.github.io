5b1bc6619e2b78ea97890d0064c3aff87c5f3418
ramesh900 <rameshsapkota900@gmail.com>
2025-09-25 00:56:50 -0700

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import { router } from "./routes/Router";
 import { RouterProvider } from "react-router-dom";
 import "../src/../index.css";
 
+// Add this block before rendering
+const params = new URLSearchParams(window.location.search);
+const path = params.get('p');
+if (path) {
+  window.history.replaceState({}, '', path);
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
